382a89cac032d234bb621fb13969aafd9b32ad5f
Jake Frasier <jake@artunlimitedusa.com>
2020-05-07 10:24:01 -0500

    adding hover events to front page boxes

--- assets/js/src/front-page.js
+++ assets/js/src/front-page.js
@@ -1,3 +1,4 @@
+//click events
 document.querySelector('.roofing').addEventListener('click', function() {
     location.assign(window.location.href + "/residential-repair");
 })
@@ -12,4 +13,37 @@ document.querySelector('.gutter').addEventListener('click', function() {
 })
 document.querySelector('.doors').addEventListener('click', function() {
     location.assign(window.location.href + "/doors");
+})
+
+//hover events
+document.querySelector('.roofing').addEventListener('mouseover', function() {
+    this.style.background = "#AAABAB";
+})
+document.querySelector('.siding').addEventListener('mouseover', function() {
+    this.style.background = "#AAABAB";
+})
+document.querySelector('.windows').addEventListener('mouseover', function() {
+    this.style.background = "#AAABAB";
+})
+document.querySelector('.gutter').addEventListener('mouseover', function() {
+    this.style.background = "#AAABAB";
+})
+document.querySelector('.doors').addEventListener('mouseover', function() {
+    this.style.background = "#AAABAB";
+})
+
+document.querySelector('.roofing').addEventListener('mouseout', function() {
+    this.style.background = "white";
+})
+document.querySelector('.siding').addEventListener('mouseout', function() {
+    this.style.background = "white";
+})
+document.querySelector('.windows').addEventListener('mouseout', function() {
+    this.style.background = "white";
+})
+document.querySelector('.gutter').addEventListener('mouseout', function() {
+    this.style.background = "white";
+})
+document.querySelector('.doors').addEventListener('mouseout', function() {
+    this.style.background = "white";
 })
